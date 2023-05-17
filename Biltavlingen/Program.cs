2d049205d84a0de1687aaae8640ed19d6eede78f
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-05-17 11:13:07 +0200

    initial commit

---
+++ Biltavlingen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Biltavlingen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
