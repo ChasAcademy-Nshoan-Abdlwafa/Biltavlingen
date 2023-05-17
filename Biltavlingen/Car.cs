d5cf12158c0ca7132d8b4e8245f45b2b9b5828b1
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-05-17 18:37:21 +0200

    Car.cs created and established some methods

---
+++ Biltavlingen/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Biltavlingen
+{
+    public class Car
+    {
+        public int? car_id { get; set; }
+
+        public string? car_name { get; set; }
+
+        public decimal car_speed { get; set; }
+
+        public int? car_distance { get; set; }
+    }
+}
--- Biltavlingen/Program.cs
+++ Biltavlingen/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Biltavlingen
+﻿using Car;
+using System;
+using System.Threading.Tasks;
+
+namespace Biltavlingen
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            await StartRace();
+        }
+
+        public static async Task StartRace()
+        {
+            Console.WriteLine("Welcome! Press any key to start the race.");
+            Console.ReadLine();
+
+            Car Car1 = new Car
+            {
+                car_id = 1,
+                car_name = "Car #1",
+                car_speed = 100,
+                car_distance = 0
+            };
+
+            Car Car2 = new Car
+            {
+                car_id = 2,
+                car_name = "Car #2",
+                car_speed = 100,
+                car_distance = 0
+            };
+
+            var carRace1 = CarRace(Car1);
+            var carRace2 = CarRace(Car2);
+            var carRaceStatus = CarStatus(new List<Car> { Car1, Car2 });
+
+            var carRaces = new List<Task> { carRace1, carRace2, carRaceStatus };
         }
     }
 }
